f8aae0787b1a2f1a7d404c646f63c270f915d7bf
MURATEC <MURATEC@dongqiaoyu>
2020-03-19 18:56:02 +0800

    20200319 dqy hostCommInfo

---
+++ src/main/java/net/muratec/mcs/entity/info/ReqGetHostCommListValidateEntity.java
@@ -0,0 +1,49 @@
+//@formatter:off
+/**
+ ******************************************************************************
+ * @file        ReqGetStockerInfoListValidateEntity.java
+ * @brief       Stocker IDリスト取得用リクエストエンティティ
+ * @par
+ * @author      DONG
+ * $Id:         $
+ * @attention
+ *
+ * Copyright (c) 2016 MURATA MACHINERY,LTD. All rights reserved.
+ *
+ * @note        Tabstop=4
+ * ----------------------------------------------------------------------------
+ * DATE       VER.        DESCRIPTION                                    AUTHOR
+ * ----------------------------------------------------------------------------
+ * 2020.03.10   Ver2.0	  MCSV4　GUI開発   							董 天津村研
+ ******************************************************************************
+ */
+//@formatter:on
+package net.muratec.mcs.entity.info;
+
+import lombok.Getter;
+import lombok.Setter;
+import net.muratec.mcs.annotation.FieldNameKey;
+import net.muratec.mcs.entity.common.AjaxDataTablesReqBaseEntity;
+import net.muratec.mcs.entity.common.AjaxReqBaseEntity;
+
+//@formatter:off
+/**
+ ******************************************************************************
+ * @brief    HOST_NAME,COMM_STATE リスト取得用リクエストエンティティクラス
+ * @par
+ * @attention
+ * @note
+ * ----------------------------------------------------------------------------
+ * VER.        DESCRIPTION                                               AUTHOR
+ * ----------------------------------------------------------------------------
+ * 2020.03.12 	ReqGetHostCommListValidateEntity 					董 天津村研
+ ******************************************************************************
+ */
+//@formatter:on
+@Getter
+@Setter
+public class ReqGetHostCommListValidateEntity extends AjaxDataTablesReqBaseEntity {
+
+	@FieldNameKey(key = "II-009-04-001") public String hostName ;
+    @FieldNameKey(key = "II-009-04-002") public String commState;
+}
