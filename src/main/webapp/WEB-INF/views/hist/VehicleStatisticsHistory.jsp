6cf2be4121714443aa2da18d7f838c3d25afde18
ZX <ZX@songgaopeng>
2020-04-15 11:45:47 +0800

    s

---
+++ src/main/webapp/WEB-INF/views/hist/VehicleStatisticsHistory.jsp
@@ -0,0 +1,157 @@
+﻿<%--
+ ******************************************************************************
+ * @file        VehicleStatisticsHistory.jsp
+ * @brief       VehicleStatisticsHistoryory　画面用JSP
+ * @par
+ * @author      天津村研　SGP
+ * $Id:         $
+ * @attention
+ *
+ * Copyright (c) 2020 MURATA MACHINERY,LTD. All rights reserved.
+ *
+ * @note
+ * ----------------------------------------------------------------------------
+ * DATE       VER.        DESCRIPTION                 AUTHOR
+ * ----------------------------------------------------------------------------
+ * 2020/04/10 v1.0.0      初版作成                                  天津村研　SGP
+ ******************************************************************************
+--%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<%@taglib uri="http://www.springframework.org/tags" prefix="spring"%>
+<%@ page session="false"%>
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<%-- 共通ヘッダ --%>
+<%@ include file="../common/commonHeader.jsp"%>
+<%-- グリッドヘッダ --%>
+<%@ include file="../common/dataTablesHeader.jsp"%>
+<%-- 更新テーブルヘッダ --%>
+<%@ include file="../common/modifyTableHeader.jsp"%>
+<%-- IdListSelectヘッダ --%>
+<%@ include file="../common/idListSelectHeader.jsp"%>
+<%-- マルチセレクトボックスヘッダ --%>
+<%@ include file="../common/multiSelectBoxHeader.jsp"%>
+
+<%-- 画面固有ヘッダ --%>
+<title><spring:message code="IH-004-01-001" /></title>
+<link rel="stylesheet" href="<c:url value='/resources/css/hist/mcs-VehicleStatisticsHistory.css?Ver=${version}'/>" media="all">
+<script src="<c:url value='/resources/js/hist/mcs-VehicleStatisticsHistory.js?Ver=${version}'/>"></script>
+<!-- <script src="<c:url value='/resources/js/component/mcs-DataTables-BgColor.js?Ver=${version}'/>"></script> -->
+
+<script>
+  var screenText = {
+      btnText: {
+    	  search: 	'<spring:message code="IH-004-02-001"/>',
+      	downLoad:   '<spring:message code="IH-004-02-002"/>',
+          cancel: 	'<spring:message code="IH-004-02-003"/>'
+    },
+    slideSearch: {
+    	tscId:	   	'<spring:message code="IH-004-03-001" />',
+    	vehicleId: 	'<spring:message code="IH-004-03-002" />',
+    	unit: 		'<spring:message code="IH-004-03-003" />',
+    	dateTime: 	'<spring:message code="IH-004-03-004" />',
+    	dateFrom: 	'<spring:message code="IH-004-03-005" />',
+    	dateTo: 	'<spring:message code="IH-004-03-006" />',
+        extract:    '<spring:message code="IH-004-03-007" />',
+        clear:      '<spring:message code="IH-004-03-008" />',
+        ret: 	    '<spring:message code="IH-004-03-009" />'
+        
+    },
+    downLoadText: {
+        saveStart: '<spring:message code="IH-004-04-002"/>',
+        saveEnd:   '<spring:message code="IH-004-04-003"/>'
+    },
+    downLoadBtn: {
+    	 saveConfirm: '<spring:message code="IH-004-04-004"/>',
+         saveReturn:  '<spring:message code="IH-004-04-005"/>'
+    }
+  };
+
+  var screenValue = {
+	  tscId: 		JSON.parse('${IH_004_01_001}'),
+	  vehicleId: 	JSON.parse('${IH_004_01_002}'),
+	  unit: 		JSON.parse('${IH_004_01_003}')
+  };
+</script>
+<%-- デザイン適用ヘッダ --%>
+<%@ include file="../common/designHeader.jsp"%>
+</head>
+
+<!-- <body onLoad="setTimeout('check()',100)"> -->
+<body>
+
+    <header id="mcs-header-menu">
+        <h1>
+            <spring:message code="IH-004-01-001" />
+        </h1>
+        <%@ include file="../common/headerCommonMenu.jsp"%>
+    </header>
+    
+
+    <!-- 一覧画面ヘッダー -->
+    <div id="mcs-subheader-menu">
+        <table>
+            <tr>
+                <td style="color:#6495ED;"><spring:message code="IH-004-01-003" /> &nbsp &nbsp</td>
+                <td style="color:#6495ED;"><div id="searchInfo"></div></td>
+            </tr>
+        </table>
+    </div>
+    
+     <!--  右メニュースライド -->
+    <nav id="mcs-right-menu">
+        <!-- 一覧 -->
+        <div id="list-btn-search" class="btn-mcs-slide" data-auth="${H004_REF}"></div>
+        <div id="list-btn-downLoad"  class="btn-mcs-slide" data-auth="${H004_REF}"></div>
+        <div id="list-btn-ret"    class="btn-mcs-slide btn-mcs-slide-return"></div>
+    </nav>
+
+    <div class="mcs-atomic-activity-history-content">
+        <!-- 一覧画面 start -->
+        <div id="list-screen" class="mcs-content mcs-with-subheader mcs-with-subtitle">
+            <div class="mcs-content-subtitle">
+                <spring:message code="IH-004-01-002" />
+            </div>
+            <div id="list-table-target" style="width: 100%; height: 100%;"> </div>
+        </div>
+        <!-- 一覧画面 end -->
+    </div>
+    <!-- スライドメニュー 検索用 -->
+    <div id="mcs-slideMenu-search">
+        <spring:message code="IH-004-03-001" />
+        <div id="mcs-search-tscId"></div>
+        
+        <spring:message code="IH-004-03-002" />
+        <div id="mcs-search-vehicleId"></div>
+        
+        <spring:message code="IH-004-03-003" />
+        <div id="mcs-search-unit"></div>
+        
+        <spring:message code="IH-004-03-004" />
+        <div id="mcs-search-dFrom"></div>
+        <div id="mcs-search-dTo"></div>
+        
+        <!-- ボタン類 -->
+        <div id="mcs-search-extract" class="btn-mcs-slide btn-mcs-slide-extact"></div>
+        <div id="mcs-search-clear" class="btn-mcs-slide btn-mcs-slide-clear"></div>
+        <div id="mcs-search-cancel" class="btn-mcs-slide btn-mcs-slide-return"></div>
+    </div>
+    
+	<!-- スライドメニュー(CSV保存) start-->
+    <div id="mcs-saveMenu">
+        <div id="mcs-saveStartDatetime"></div>
+        <div id="mcs-saveEndDatetime"></div>
+        <div id="btn-saveConfirm" class="btn-mcs-slide btn-mcs-slide-confirm"></div>
+        <div id="btn-saveReturn" class="btn-mcs-slide btn-mcs-slide-return"></div>
+    </div>
+    <!-- スライドメニュー(CSV保存) end -->
+    
+    <div id="mcs-alert-dialog"></div>
+    <div id="mcs-confirm-dialog"></div>
+    <div id="mcs-information-dialog"></div>
+    <!-- ダイアログ -->
+    <div id="mcs-error-dialog"></div>
+</body>
+</html>
---
+++ src/main/webapp/resources/js/hist/mcs-VehicleStatisticsHistory.js
@@ -0,0 +1,431 @@
+﻿/**
+ ******************************************************************************
+ * @file        mcs-VehicleStatisticsHistory.js
+ * @brief       StatisticsHistory(Vehicle) 画面用JavaScript
+ * @par
+ * @author      天津村研　SGP
+ * $Id:         $
+ * @attention
+ *
+ * Copyright (c) 2020 MURATA MACHINERY,LTD. All rights reserved.
+ *
+ * @note        Tabstop=2
+ * ----------------------------------------------------------------------------
+ * DATE       VER.        DESCRIPTION                           AUTHOR
+ * ----------------------------------------------------------------------------
+ * 2020/04/10 v1.0.0      初版作成                                      　　　　天津村研　SGP
+ ******************************************************************************
+ */
+$(function() {
+	
+  focus();
+  // コンポーネントマネージャ（検索用）
+  var searchComp = new McsComponentManager();
+
+  // 非アクティブ状態でも自動更新を行う
+  AutoReloadTimerManager.setEnableBlurExecute();
+
+  // 画面の番号定義
+  const
+  SCREEN = {
+	SEARCH:    0,
+	DOWNLOAD:  1,
+    RETURN: 2
+  };
+
+  // 現在表示している画面の番号
+  var screenIndex;
+
+  // スライドメニュー生成
+  var slideMenuTop = McsSlideMenu.primaryMenuSlide;
+  //検索
+  var slideMenuSearch = new McsSlideMenu({
+    depth: 1,
+    parent: slideMenuTop,
+    slideDiv: $('#mcs-slideMenu-search')
+  });
+//CSV保存用スライドの初期化
+  var saveMenu = new McsSlideMenu({
+    depth: 1,
+    parent: null,
+    slideDiv: $('#mcs-saveMenu')
+  });
+  
+  // テーブル
+  //true:複数行を選択できる；false：単数行を選択する
+  //  var dataTables = new McsDataTables($('#list-table-target'), true);
+  var dataTables = new McsDataTables($('#list-table-target'), false);
+ 
+  //戻るボタン押下時にスライドを閉じないようにするためのフラグ
+  var retFlag = false;
+  
+  /**
+   * ダイアログの生成
+   */
+  var errorDialog = new McsDialog($('#mcs-error-dialog'), window.mcsDialogTitleError);
+  //初期表示処理
+  // ---------------------------------------
+  
+  //右スライドメニュー生成メソッド
+  creTopMenu();
+  
+  //初回検索
+  extract({
+	  tscId: '',
+	  vehicle: '',
+	  unit: '',
+	  dateFrom: null,
+	  dateTo: null,
+	  maxRecords: ''
+  });
+  
+  //検索ボタンを設定する
+  createSearchSlide();
+  // CSV保存用スライドの生成
+  saveCsvSlide();
+  
+  /**
+   ******************************************************************************
+   * @brief   抽出して画面へ表示する
+   * @param   {Object} cond 抽出条件（そのままサーバへ送信されるJSON）
+   * @return
+   * @retval
+   * @attention
+   * @note
+   * ----------------------------------------------------------------------------
+   * VER.        DESCRIPTION                                               AUTHOR
+   * ----------------------------------------------------------------------------
+   ******************************************************************************
+   */
+  function extract(cond) {
+	  //searchComp.clearErrors();
+	  
+	  dataTables.getDataAjax({
+		  url: getUrl('/VehicleStatisticsHistory/GetVehicleStatisticsHistoryList'),
+		  cond: cond,
+		  searchDataFlag: true,
+		  tableCompId: 'H-004-VehicleStatisticsHistoryList', // テーブルコンポーネントID
+		  success: function(data) {
+			  // 成功時
+			  // 特にすることなし
+			  if (retFlag) {
+				  // 戻るボタンが押されたときは閉じない
+				  // 戻るボタン用フラグを下す
+				  retFlag = false;
+				  return;
+			  }
+		  },
+		  serverError: function(data) {
+			  // 特にすることなし
+			  //searchComp.setErrors(data.result.errorInfoList);
+		  },
+		  ajaxError: function(status, error) {
+			  // 特にすることなし
+		  }
+	  });
+  }
+
+  //showListScreen(false);
+  // 一覧画面のデータ取得、表示
+  // 自動更新有効化
+  /*AutoReloadTimerManager.addTimeoutCallback(function() {
+    	//showListScreen(false);
+    	AutoReloadTimerManager.start();
+  });
+  AutoReloadTimerManager.start();*/
+
+  /**
+   ******************************************************************************
+   * @brief   右スライドメニュー生成メソッド
+   * @param
+   * @return
+   * @retval
+   * @attention
+   * @note    ボタン等のコンポーネント生成を行い、各処理を設定。
+   * ----------------------------------------------------------------------------
+   * VER.        DESCRIPTION                                               AUTHOR
+   * ----------------------------------------------------------------------------
+   ******************************************************************************
+   */
+  function creTopMenu() {
+    // ボタン生成
+    var searchBtn = new McsButton($('#list-btn-search'), screenText.btnText.search);
+    var downLoadBtn = new McsButton($('#list-btn-downLoad'), screenText.btnText.downLoad);
+    var rtnBtn = new McsButton($('#list-btn-ret'), screenText.btnText.cancel);
+
+   /* // 再表示ボタン押下処理
+    reloadBtn.onClick(function() {
+        // エラー表示をクリア
+        dataTables.reload();
+        retFlag = true;
+    });*/
+
+    // 戻るボタン押下処理
+    rtnBtn.onClick(function() {
+      slideMenuTop.toggle();
+    });
+    // 検索ボタン押下
+    searchBtn.onClick(function() {
+      // 画面の内容を消去
+//      searchComp.clearErrors();
+
+      /*// 前回の条件を復元
+      var datas = dataTables.getLatestCond();
+
+      searchComp.get('currentTscId').setValue(datas.currentTscId);
+      searchComp.get('carrierId').setValue(datas.carrierId);*/
+
+      slideMenuSearch.show();
+    });
+    
+    //download
+    downLoadBtn.onClick(function() {
+    	saveMenu.show();
+    });
+  }
+  /**
+   ******************************************************************************
+   * @brief   検索メニュー生成
+   * @param
+   * @return
+   * @retval
+   * @attention
+   * @note
+   * ----------------------------------------------------------------------------
+   * VER.        DESCRIPTION                                               AUTHOR
+   * ----------------------------------------------------------------------------
+   * MACS4#0047  GUI要望分                                              T.Iga/CSC
+   ******************************************************************************
+   */
+  function createSearchSlide() {
+	  
+	searchComp.clearErrors();
+	// 表示画面番号の更新
+	   screenIndex = SCREEN.SEARCH;
+    // 検索項目の生成
+    var tscId  = new McsSelectBox($('#mcs-search-tscId'));
+    var vehicleId = new McsSelectBox($('#mcs-search-vehicleId'));
+    var unit = new McsSelectBox($('#mcs-search-unit'));
+    var dateFrom = new McsDateTime($('#mcs-search-dFrom'), screenText.slideSearch.dateFrom, 75,true);
+    var dateTo = new McsDateTime($('#mcs-search-dTo'), screenText.slideSearch.dateTo, 75,true);
+    var extract = new McsButton($('#mcs-search-extract'), screenText.slideSearch.extract);
+    var clear = new McsButton($('#mcs-search-clear'), screenText.slideSearch.clear);
+    var ret = new McsButton($('#mcs-search-cancel'), screenText.slideSearch.ret);
+    
+    var tscIdList = screenValue.tscId;
+    var vehicleIdList= screenValue.vehicleId;
+    var unitList= screenValue.unit;
+    
+    // コンポーネントマネージャーに各検索項目を入れる
+    searchComp.add('tscId', tscId);
+    searchComp.add('vehicleId', vehicleId);
+    searchComp.add('unit', unit);
+    searchComp.add('dateFrom', dateFrom);
+    searchComp.add('dateTo', dateTo);
+    
+    tscId.setList(tscIdList);
+    vehicleId.setList(vehicleIdList);
+    unit.setList(unitList);
+   /* 
+   // tscId変更時にsource、destinationの要素を取得し、一覧を再検索
+    tscId.onChange(function() {
+      setSourceList(tscId.getValue());
+      setDestinationList(tscId.getValue());
+    });
+   */   
+    // 抽出ボタン押下
+    extract.onClick(function() {
+      
+    // エラー解除
+    searchComp.clearErrors();
+      
+      
+      // 検索処理
+      var url = getUrl('/VehicleStatisticsHistory/GetVehicleStatisticsHistoryList');
+      var cond = {
+    		  tscId: tscId.getValue(),
+    		  vehicleId: vehicleId.getValue(),
+    		  unit: unit.getValue(),
+    		  dateFrom: dateFrom.getValue(),
+    		  dateTo: dateTo.getValue()
+      };
+      
+      var space = "&nbsp&nbsp"; 
+      var searchInformation = "";
+      var searchInfoTscIds = "";
+      var searchInfovehicleId = "";
+      var searchInfoUnits = "";
+      var searchInfoDateFroms = "";
+      var searchInfoDateTos = "";
+      var searchInfoTscId = tscId.getText();
+      var searchInfoVehicleId = vehicleId.getText();
+      var searchInfoUnit = unit.getText();
+      var searchInfoDateFrom = dateFrom.getValue();
+      var searchInfoDateTo = dateTo.getValue();
+      
+      if(searchInfoTscId == "All"){
+    	  searchInfoTscId = "";
+      }
+      if(searchInfoVehicleId == "All"){
+    	  searchInfoVehicleId = "";
+      }
+    
+      if(searchInfoTscId!=null && searchInfoTscId !="")
+      {
+    	  searchInfoTscIds = " TSCID ["+searchInfoTscId+"]" + space ;
+      }
+      if(searchInfoVehicleId!=null && searchInfoVehicleId !="")
+      {
+    	  searchInfoVehicleId = " Source " + "[" + searchInfoVehicleId + "]" + space ;
+      }
+      if(searchInfoUnit!=null && searchInfoUnit !="")
+      {
+    	  searchInfoUnits = " Unit ["+searchInfoUnit+"]" + space ;
+      }
+      if(searchInfoDateFrom!=null && searchInfoDateFrom !="")
+      {
+    	  searchInfoDateFroms = " Date&Time " + "[" + searchInfoDateFrom + " - " ;
+      }
+      if(searchInfoDateTo!=null && searchInfoDateTo !="")
+      {
+    	  searchInfoDateTos = searchInfoDateTo + "]" + space ;
+      }
+      
+      $('#searchInfo').html(  searchInfoTscIds+
+    		  				  searchInfoVehicleId + 
+				    		  searchInfoUnits +
+				    		  searchInfoDateFroms + 
+				    		  searchInfoDateTos );
+      
+      var tableCompId = 'H-004-vehicleStatisticsHistoryList';
+      var options = {
+        url: url,
+        cond: cond,
+        searchDataFlag: true,
+        tableCompId: tableCompId,
+        success: function() {
+          // 検索成功時
+          if (retFlag) {
+        	
+            // 戻るボタン押下時
+            // 戻るボタン用フラグを下す
+            retFlag = false;
+            return;
+          }
+          //firstSearchFlag = false;
+        },
+        serverError: function(result) {
+          // 検索失敗時
+          searchComp.setErrors(result.result.errorInfoList);
+        },
+        ajaxError: function() {
+          // Ajaxエラー時
+        }
+
+      };
+      dataTables.getDataAjax(options);
+    });
+ // クリアボタン押下
+    clear.onClick(function() {
+      // 各項目を初期化する
+    	tscId.clear();
+    	vehicleId.clear();
+    	unit.clear();
+    	dateFrom.clear();
+    	dateTo.clear();
+    });
+    // 戻るボタン押下
+    ret.onClick(function() {
+      slideMenuSearch.hide();
+    });
+
+    /**
+     * 検索メニュー生成終了
+     */
+  }
+  /**
+   ******************************************************************************
+   * @brief   状態画面の表示クリア機能
+   * @param
+   * @return
+   * @retval
+   * @attention
+   * @note     状態画面のコンポーネントをクリアする。
+   * ----------------------------------------------------------------------------
+   * VER.        DESCRIPTION                                               AUTHOR
+   * ----------------------------------------------------------------------------
+   ******************************************************************************
+   */
+  function clearState() {
+    // 状態画面のテーブルをクリア
+	dataTables.clear();
+  }
+  
+  /**
+   ******************************************************************************
+   * @brief   CSV保存用スライドを生成
+   * @param
+   * @return
+   * @retval
+   * @attention
+   * @note
+   * ----------------------------------------------------------------------------
+   * VER.        DESCRIPTION                                               AUTHOR
+   * ----------------------------------------------------------------------------
+   ******************************************************************************
+   */
+  function saveCsvSlide() {
+	  
+	// 表示画面番号の更新
+	   screenIndex = SCREEN.DOWNLOAD;
+	   
+    // ******************************************************
+    // 検索項目生成
+    // ******************************************************
+    // datetimePicker生成
+    // 開始の項目
+    var saveStart = new McsDateTime($('#mcs-saveStartDatetime'), screenText.downLoadText.saveStart, 75);
+    // 終了の項目
+    var saveEnd = new McsDateTime($('#mcs-saveEndDatetime'), screenText.downLoadText.saveEnd, 75);
+
+    // 非活性項目の設定
+    saveStart.setEnabled(false);
+    saveEnd.setEnabled(false);
+
+    // 物件対応
+    saveStart.hide();
+    saveEnd.hide();
+
+    // ******************************************************
+    // ボタン生成
+    // ******************************************************
+    // 決定ボタン
+    var saveConfirmButton = new McsButton($('#btn-saveConfirm'), screenText.downLoadBtn.saveConfirm);
+
+    // CSV保存の戻るボタン
+    var saveReturnButton = new McsButton($('#btn-saveReturn'), screenText.downLoadBtn.saveReturn);
+
+    // ******************************************************
+    // 各イベント
+    // ******************************************************
+    // 決定ボタン押下
+    saveConfirmButton.onClick(function() {
+      var datas = dataTables.getLatestCond();
+      callAjax(getUrl('/VehicleStatisticsHistory/SetCsvVehicleStatisticsHistoryList'), datas, false,
+      // 成功
+      function(retObj) {
+        window.location.href = getUrl('/VehicleStatisticsHistory/SaveCsvVehicleStatisticsHistoryList');
+      },
+      // エラー
+      function(retObj) {
+        // 特にすることなし
+      });
+    });
+
+    // 戻るボタン押下
+    saveReturnButton.onClick(function() {
+      saveMenu.hide();
+    });
+  }
+
+});
