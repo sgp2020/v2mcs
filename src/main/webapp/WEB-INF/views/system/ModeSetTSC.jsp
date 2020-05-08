fc0b51776b37ca666dd48abb8d717bb881e52be1
MURATEC03 <MURATEC03@MURATEC03>
2020-05-08 14:42:50 +0800

    dqy

---
+++ src/main/webapp/WEB-INF/views/system/ModeSetTSC.jsp
@@ -0,0 +1,195 @@
+﻿<%--
+ ******************************************************************************
+ * @file        ModeSetTSC.jsp
+ * @brief       ModeSetTSC　画面用JSP
+ * @par
+ * @author      天津村研　董
+ * $Id:         $
+ * @attention
+ *
+ * Copyright (c) 2016 MURATA MACHINERY,LTD. All rights reserved.
+ *
+ * @note
+ * ----------------------------------------------------------------------------
+ * DATE       VER.        DESCRIPTION                                    AUTHOR
+ * ----------------------------------------------------------------------------
+ * 2020/05/07 v1.0.0      初版作成                                      　　　　　　　　　　　　　　　　　　                    天津村研　董
+ ******************************************************************************
+--%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<%@taglib uri="http://www.springframework.org/tags" prefix="spring"%>
+<%@ page session="false"%>
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<%-- 共通ヘッダ --%>
+<%@ include file="../common/commonHeader.jsp"%>
+<%-- グリッドヘッダ --%>
+<%@ include file="../common/dataTablesHeader.jsp"%>
+<%-- 更新テーブルヘッダ --%>
+<%@ include file="../common/modifyTableHeader.jsp"%>
+<%-- IdListSelectヘッダ --%>
+<%@ include file="../common/idListSelectHeader.jsp"%>
+<%-- マルチセレクトボックスヘッダ --%>
+<%@ include file="../common/multiSelectBoxHeader.jsp"%>
+
+<%-- 画面固有ヘッダ --%>
+<title><spring:message code="IS-001-01-001" /></title>
+<link rel="stylesheet" href="<c:url value='/resources/css/info/mcs-ModeSetTSC.css?Ver=${version}'/>" media="all">
+<script src="<c:url value='/resources/js/info/mcs-ModeSetTSC.js?Ver=${version}'/>"></script>
+<script src="<c:url value='/resources/js/component/mcs-DataTables-BgColor.js?Ver=${version}'/>"></script>
+
+<script>
+  var screenText = {
+    btnText: {
+    	modeSet: '<spring:message code="IS-001-01-020"/>',
+    	modeSetHistory: '<spring:message code="IS-001-01-021"/>',
+    	color: '<spring:message code="IS-001-01-022"/>',
+        reload: '<spring:message code="IS-001-01-023"/>',
+        cancel: '<spring:message code="IS-001-01-024"/>'
+    },
+    slideModeSet: {
+    	currentState:'<spring:message code="IS-001-02-002" />',
+    	changeState: '<spring:message code="IS-001-02-003" />',
+    	Comment:	 '<spring:message code="IS-001-02-004" />',
+        extract:     '<spring:message code="IS-001-02-005" />',
+        clear:       '<spring:message code="IS-001-02-006" />',
+        ret: 	     '<spring:message code="IS-001-02-007" />'
+        
+    },
+    slideModeSetHistory: {
+    	tscName:	 '<spring:message code="IS-001-04-002" />',
+    	currentMode: '<spring:message code="IS-001-04-003" />',
+        ret: 	     '<spring:message code="IS-001-04-011" />'
+        
+    },
+    dialog: {
+        listNotSelect: '<spring:message code="IH-002-01.001" />',
+        listRet: '<spring:message code="IH-002-01.002" />'
+    },
+    <%-- 画面にはColorを表示する--%>
+    ctrlText: {
+        Normal:  'Normal',
+        Low:     'Low',
+        High:    'High'
+      },
+      colorText: {	
+          enable :  '#00FF00',
+          disable:  '#808080',
+          test	 :  '#6E78FF',
+          PM	 :  '#FF8C00',
+          notReady:'#FF0000'
+        }
+  };
+
+  var screenValue = {
+		tscType: JSON.parse('${IS_001_01_001}')
+  };
+</script>
+
+<%-- デザイン適用ヘッダ --%>
+<%@ include file="../common/designHeader.jsp"%>
+</head>
+
+<body>
+
+    <header id="mcs-header-menu">
+        <h1>
+            <spring:message code="IS-001-01-001" />
+        </h1>
+        <%@ include file="../common/headerCommonMenu.jsp"%>
+    </header>
+
+    <div id="mcs-alert-dialog"></div>
+    <div id="mcs-confirm-dialog"></div>
+    <div id="mcs-information-dialog"></div>
+
+    <!-- 空FOUP一覧画面ヘッダー -->
+    <div id="mcs-subheader-menu">
+        <table>
+            <tr>
+                <td><spring:message code="IS-001-01-003" /></td>
+                <td class="mcs-td-selectbox">
+                    <div id="sel-ctrl"></div>
+                </td>
+            </tr>
+        </table>
+    </div>
+
+    <div class="mcs-modeSetTsc-system-content">
+
+        <!-- 一覧画面 start -->
+        <div id="list-screen" class="mcs-content mcs-with-subheader mcs-with-subtitle">
+            <div class="mcs-content-subtitle">
+                <spring:message code="IS-001-01-002" />
+            </div>
+            <div id="list-table-target"> </div>
+            <!-- スライドメニュー 検索用 -->
+		    <div id="mcs-slideMenu-modeSet">
+		        <spring:message code="IS-001-02-001" />
+		        <div id="mcs-search-hostName" style="width:200px"></div>
+		        
+		        <spring:message code="II-009-04-002" />
+		        <div id="mcs-search-commState"></div>
+		        
+		        <!-- ボタン類 -->
+		        <div id="mcs-search-extract" class="btn-mcs-slide btn-mcs-slide-extact"></div>
+		        <div id="mcs-search-clear" class="btn-mcs-slide btn-mcs-slide-clear"></div>
+		        <div id="mcs-search-cancel" class="btn-mcs-slide btn-mcs-slide-return"></div>
+		    </div>
+            <div id="ColorDiv" >
+			         <div id="colorColumn" class="remarks-content-color">
+			         	 <div id="mcs-content-Occupancy">
+			         	 	<spring:message code="IS-001-03-001" />
+			         	 </div>	
+			             <div id="color1"></div>
+			             <div id="color2"></div>
+			             <div id="color3"></div>
+			             <div id="color4"></div>
+			             <div id="color5"></div>
+			         </div>
+			         <div id="ctrlColumn" >  
+		             	  <div class="mcs-content-Enable">
+			         	 	<spring:message code="IS-001-03-002" />
+			         	 </div>	
+		             	  <div class="mcs-content-Disable">
+			         	 	<spring:message code="IS-001-03-003" />
+			         	 </div>	
+		             	 <div class="mcs-content-Test">
+			         	 	<spring:message code="IS-001-03-004" />
+			         	 </div>	
+		             	 <div class="mcs-content-PM">
+			         	 	<spring:message code="IS-001-03-005" />
+			         	 </div>	
+		             	 <div class="mcs-content-NotReady">
+			         	 	<spring:message code="IS-001-03-006" />
+			         	 </div>	
+			         </div>
+        		</div>
+        </div>
+        <!-- 一覧画面 end -->
+
+    </div>
+
+    <!--  右メニュースライド -->
+    <nav id="mcs-right-menu">
+        <!-- 一覧 -->
+        <div id="list-btn-modeSet" class="btn-mcs-slide" data-auth="${S001_REF}"></div>
+        <div id="list-btn-modeSetHistory" class="btn-mcs-slide" data-auth="${S001_REF}"></div>
+        <div id="list-btn-color"  class="btn-mcs-slide" data-auth="${S001_REF}"></div>
+        <div id="list-btn-reload" class="btn-mcs-slide" data-auth="${S001_REF}"></div>
+        <div id="list-btn-ret" class="btn-mcs-slide btn-mcs-slide-return"></div>
+    </nav>
+
+    <!-- スライドメニュー(CSV保存) start-->
+    <div id="mcs-saveMenu" class="mcs-saveMenu">
+        <div id="mcs-saveStartDatetime"></div>
+        <div id="mcs-saveEndDatetime"></div>
+        <div id="btn-confirm" class="btn-mcs-slide btn-mcs-slide-confirm"></div>
+        <div id="btn-save-ret" class="btn-mcs-slide btn-mcs-slide-return"></div>
+    </div>
+    <!-- スライドメニュー(CSV保存) end -->
+
+</body>
+</html>
---
+++ src/main/webapp/resources/js/system/mcs-ModeSetTSC.js
@@ -0,0 +1,244 @@
+﻿/**
+ ******************************************************************************
+ * @file        mcs-ModeSetTSC.js
+ * @brief       ModeSetTSC 画面用JavaScript
+ * @par
+ * @author      天津村研　董
+ * $Id:         $
+ * @attention
+ *
+ * Copyright (c) 2016 MURATA MACHINERY,LTD. All rights reserved.
+ *
+ * @note        Tabstop=2
+ * ----------------------------------------------------------------------------
+ * DATE       VER.        DESCRIPTION                                    AUTHOR
+ * ----------------------------------------------------------------------------
+ * 2020/05/08 v1.0.0      初版作成                                      　　　　　　　　　　　　　　　　　　                   天津村研　董
+ ******************************************************************************
+ */
+$(function() {
+	
+  focus();
+
+  //STD 2020.03.15 天津村研　董  MCSV4　GUI開発  Ver2.0 Rev.000 
+  // ステータス色一覧
+  $('#color1').css('background-color', screenText.colorText.Normal);
+  $('#color2').css('background-color', screenText.colorText.Low);
+  $('#color3').css('background-color', screenText.colorText.High);
+  /*var ctrl1 = new McsTextBox($('#ctrl1'));
+  var ctrl2 = new McsTextBox($('#ctrl2'));
+  var ctrl3 = new McsTextBox($('#ctrl3'));
+  ctrl1.setReadonly(true);
+  ctrl2.setReadonly(true);
+  ctrl3.setReadonly(true);
+  ctrl1.setValue(screenText.ctrlText.Normal);
+  ctrl2.setValue(screenText.ctrlText.Low);
+  ctrl3.setValue(screenText.ctrlText.High);*/
+
+  //END 2020.03.15 天津村研　董  MCSV4　GUI開発  Ver2.0 Rev.000 
+  
+  // 非アクティブ状態でも自動更新を行う
+  AutoReloadTimerManager.setEnableBlurExecute();
+
+  // 画面の番号定義
+  const
+  SCREEN = {
+	  MODESET: 0,
+	  MODESETHISTORY: 1,
+	  COLOR  : 2,
+	  RELOAD : 3,
+	  RETURN :4
+  };
+
+  // 現在表示している画面の番号
+  var screenIndex;
+
+  // tscType選択用セレクトボックス生成
+  var ctrlSelBox = new McsSelectBox($('#sel-ctrl'));
+  var tscTypeList = screenValue.tscType;
+  ctrlSelBox.setList(tscTypeList);
+
+  ctrlSelBox.onChange(function() {
+    // エラー表示をクリア
+    selComp.clearErrors();
+//    getData(ctrlSelBox.getValue(), true);
+    showListScreen(false);
+  });
+
+  // コンポーネントマネージャ
+  var selComp = new McsComponentManager();
+  selComp.add('tscId', ctrlSelBox);
+
+  // スライドメニュー生成
+  var slideMenuTop = McsSlideMenu.primaryMenuSlide;
+  creTopMenu();
+
+  // テーブル
+  //STD 2020.03.17 天津村研　董  MCSV4　GUI開発  Ver2.0 Rev.000 
+  //var dataTables = new McsDataTables($('#list-table-target'), true);
+  //20200318 DQY MOD
+  var dataTables = new McsDataTablesBgColor($('#list-table-target'), true);
+  //END 2020.03.17 天津村研　董  MCSV4　GUI開発  Ver2.0 Rev.000 
+
+  // 初期表示処理
+  // ---------------------------------------
+  ctrlSelBox.setList(screenValue.tscId);
+  showListScreen(false);
+  // 一覧画面のデータ取得、表示
+  // 自動更新有効化
+  AutoReloadTimerManager.addTimeoutCallback(function() {
+    	showListScreen(false);
+    	AutoReloadTimerManager.start();
+  });
+  AutoReloadTimerManager.start();
+
+  /**
+   ******************************************************************************
+   * @brief   右スライドメニュー生成メソッド
+   * @param
+   * @return
+   * @retval
+   * @attention
+   * @note    ボタン等のコンポーネント生成を行い、各処理を設定。
+   * ----------------------------------------------------------------------------
+   * VER.        DESCRIPTION                                               AUTHOR
+   * ----------------------------------------------------------------------------
+   ******************************************************************************
+   */
+  function creTopMenu() {
+    // ボタン生成
+    var modeSetBtn = new McsButton($('#list-btn-modeSet'), screenText.btnText.modeSet);
+    var modeSetHistoryBtn = new McsButton($('#list-btn-modeSetHistory'), screenText.btnText.modeSetHistory);
+    var colorBtn = new McsButton($('#list-btn-color'), screenText.btnText.color);
+    var reloadBtn = new McsButton($('#list-btn-reload'), screenText.btnText.reload);
+    var rtnBtn = new McsButton($('#list-btn-ret'), screenText.btnText.cancel);
+
+    // 再表示ボタン押下処理
+    reloadBtn.onClick(function() {
+        // エラー表示をクリア
+        dataTables.reload();
+        retFlag = true;
+    });
+
+    // 戻るボタン押下処理
+    rtnBtn.onClick(function() {
+      slideMenuTop.toggle();
+    });
+  }
+
+  /**
+   ******************************************************************************
+   * @brief   状態画面の表示クリア機能
+   * @param
+   * @return
+   * @retval
+   * @attention
+   * @note     状態画面のコンポーネントをクリアする。
+   * ----------------------------------------------------------------------------
+   * VER.        DESCRIPTION                                               AUTHOR
+   * ----------------------------------------------------------------------------
+   ******************************************************************************
+   */
+  function clearState() {
+    // 状態画面のテーブルをクリア
+	dataTables.clear();
+  }
+  
+  /**
+   ******************************************************************************
+   * @brief  一覧画面表示処理
+   * @param {Boolean}  reloadFlg true:直前のデータで更新 false:現在のデータで更新
+   * @return {Boolean} true
+   * @retval
+   * @attention
+   * @note
+   * ----------------------------------------------------------------------------
+   * VER.        DESCRIPTION                                               AUTHOR
+   * ----------------------------------------------------------------------------
+   ******************************************************************************
+   */
+  function showListScreen(reloadFlg) {
+
+    // 全項目非表示
+//    hideAllScreen();
+
+    //対象項目表示
+    $('#mcs-subheader-menu').show();
+    $('#list-screen').show();
+    //$('#mcs-subtitle-lst').show();
+
+    //reloadButton.show();
+    //cancelButton.show();
+
+   // changeSelectBoxList();//DQY DEL 20200313
+    ctrlSelBox.setEnabled(true);
+
+    //画面モードセット
+//    currentDisp = DISP_MODE.EMPTY_CARRIER_LIST;
+
+    if (reloadFlg) {
+      dataTables.reload();
+    } else {
+      // AJax呼び出し
+      var values = {};
+//      values.controllerId = ctrlSelBox.getValue();
+      values.tscType = ctrlSelBox.getValue();//20200313 DQY DEL
+
+      // 表示をクリア
+      clearState();
+      
+      dataTables.getDataAjax({
+        url: getUrl('/ModeSetTSC/GetModeSetTSCList'), // データ取得元
+        cond: values, // 検索条件
+        searchDataFlag: true,
+        tableCompId: 'I-005-dataTables',
+        success: function(data) {
+          // 成功時
+        },
+        serverError: function(retObj) {
+          // エラーメッセージクリア
+          //ctrlSelBox.clearErrors();
+          // エラーメッセージを付与
+          ctrlSelBox.setErrors(retObj.result.errorInfoList);
+        },
+        ajaxError: function(message, status) {
+          // 何もすることなし
+        }
+      });
+    }
+
+    return true;
+  }
+  /**
+   *******************************************************************************
+   * @brief       空FOUP一覧 コントローラIDセレクトボックス リスト更新処理
+   * @param
+   * @return
+   * @retval
+   * @attention
+   * @note
+   * ----------------------------------------------------------------------------
+   * VER.        DESCRIPTION                                               AUTHOR
+   * ----------------------------------------------------------------------------
+   *******************************************************************************
+   */
+  /*function changeSelectBoxList() {
+    var url = getUrl('/StockerInfo/GetStockerSelectBoxList');
+    var cond = {};
+    var datas = [];
+
+    var success = function(retObj) {
+      // コントローラID セレクトボックス
+      var values = {};
+      var selControllerId = ctrlSelBox.getValue();
+      ctrlSelBox.clear();
+//      ctrlSelBox.setList(retObj.controllerIdList);
+      ctrlSelBox.setList(retObj.tscIdList);
+      ctrlSelBox.setValue(selControllerId);
+    }
+    callAjax(url, JSON.stringify(cond), false, success);
+
+  }*/
+  
+  
+});
